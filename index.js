d65bd819bb393c6ef10d1d9d6374ded30682cbe3
Kartik Vanjani <you@example.com>
2024-06-13 14:55:12 -0400

    characters des done

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 let items = document.querySelectorAll('.slider .list .item');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
-let thumbnails = document.querySelectorAll('.thumbnail .item');
+let thumbnail = document.querySelectorAll('.thumbnail .item');
 
 let item_count = items.length;
 let act = 0;
 
 next.onclick = function() {
-    act = (act + 1) % item_count;
+    act = act + 1;
+    if (act >= item_count) {
+        act = 0;
+    }
     showSlider();
 }
 
 prev.onclick = function() {
-    act = (act - 1 + item_count) % item_count;
+    act = act - 1;
+    if (act < 0) {
+        act = item_count - 1;
+    }
     showSlider();
 }
 
 function showSlider() {
-    let oldItem = document.querySelector('.slider .list .item.active');
-    let oldThumbnail = document.querySelector('.thumbnail .item.active');
-
-    oldItem.classList.remove('active');
-    oldThumbnail.classList.remove('active');
+    let old = document.querySelector('.slider .list .item.active');
+    let oldT = document.querySelector('.thumbnail .item.active');
+    old.classList.remove('active');
+    oldT.classList.remove('active');
 
     items[act].classList.add('active');
-    thumbnails[act].classList.add('active');
+    thumbnail[act].classList.add('active');
 }
